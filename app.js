b608364f565f59dda77475e94f13ab922d82453e
Alexei Kosykh <aleksei-kosykh@mail.ru>
2021-03-20 17:54:45 +0300

    relized without styles

--- app.js
+++ app.js
@@ -1,17 +1,48 @@
 let btnSend = document.querySelector("#btnSend");
 let dataUserGeneral = {
-  Reading: 0,
-  Laziness: 0,
-  Relax: 0,
-  Sleeping: 0,
-  Working: 0,
-  Studing_ENG: 0,
-  Studing_JS: 0,
-  Watching_FILMS: 0,
-  Walking: 0,
-  Playing_Games: 0,
+  Reading: 2,
+  Laziness: 6,
+  Relax: 3,
+  Sleeping: 2,
+  Working: 4,
+  Studing_ENG: 5,
+  Studing_JS: 4,
+  Watching_FILMS: 2,
+  Walking: 7,
+  Playing_Games: 10,
 };
-let dataUserWithDate = {};
+
+let dataUserWithDate = {
+  "9.3.2021": { Reading: 2, Playing_Games: 3 },
+  "10.3.2021": {
+    Reading: 2,
+    Laziness: 6,
+    Relax: 3,
+    Sleeping: 2,
+    Working: 1,
+    Studing_ENG: 5,
+    Studing_JS: 4,
+    Watching_FILMS: 2,
+    Walking: 7,
+    Playing_Games: 10,
+  },
+  "11.3.2021": {
+    Reading: 5,
+    Laziness: 5,
+    Relax: 3,
+    Sleeping: 2,
+    Working: 1,
+    Studing_ENG: 9,
+    Studing_JS: 10,
+    Watching_FILMS: 1,
+    Walking: 4,
+    Playing_Games: 11,
+  },
+  "12.3.2021": { Reading: 2 },
+  "13.3.2021": { Watching_FILMS: 2 },
+  "15.3.2021": { Reading: 10 },
+  "17.3.2021": { Playing_Games: 10 },
+};
 
 localStorage.setItem("dataUserGeneral", JSON.stringify(dataUserGeneral));
 localStorage.setItem("dataUserWithDate", JSON.stringify(dataUserWithDate));
@@ -36,14 +67,14 @@ const drawHorBarDiagram = (category, amount, arrOpt) => {
         {
           barPercentage: 1.2, // ширина линий
           backgroundColor: [
-            "rgb(155, 199, 132,0.3)",
-            "rgba(255, 99, 132, 0.3)",
-            "rgba(54, 162, 235, 0.3)",
-            "rgba(255, 206, 86, 0.3)",
-            "rgba(75, 192, 192, 0.3)",
-            "rgba(153, 102, 255, 0.3)",
-            "rgba(255, 159, 64, 0.3)",
-            "rgba(9, 129, 45, 0.3)",
+            "rgb(203, 67, 53,0.6)",
+            "rgb(155, 89, 182,0.6)",
+            "rgb(37, 113, 163, 0.6)",
+            "rgba(255, 206, 86, 0.6)",
+            "rgb(22, 159, 133,0.6)",
+            "rgb(214, 137, 16,0.6)",
+            "rgb(112, 123, 124,0.6)",
+            "rgb(46, 64, 83,0.6)",
             "rgba(15, 0, 100, 0.3)",
           ],
           hoverBackgroundColor: [
@@ -81,6 +112,15 @@ const drawHorBarDiagram = (category, amount, arrOpt) => {
       legend: {
         display: false,
       },
+      scales: {
+        xAxes: [
+          {
+            ticks: {
+              beginAtZero: true,
+            },
+          },
+        ],
+      },
     },
   });
 };
@@ -106,13 +146,23 @@ const checkArrayWithDate = (date, value, hours) => {
   }
 };
 
-const drawLineDiagram = () => {
+const drawLineDiagram = (value, hours) => {
   let ctx = document.getElementById("diagramLinear").getContext("2d");
+  let datesAll = Object.keys(dataUserWithDate);
+  console.log(
+    Object.values(dataUserWithDate).map(function (item) {
+      if (item.Playing_Games) {
+        return item.Playing_Games;
+      }
+      return 0;
+    })
+  );
   let daysData = {
-    labels: [2, 12, 21, 12, 21, 21, 21], // дата
+    labels: datesAll,
+
     datasets: [
       {
-        label: "Car Speed",
+        label: `${Object.keys(dataUserGeneral)[0]}`,
         fill: true,
         lineTension: 0.1,
         backgroundColor: "rgba(75,192,192,0.2)",
@@ -127,10 +177,15 @@ const drawLineDiagram = () => {
         pointHoverBorderColor: "rgba(220,220,220,1)",
         pointRadius: 1,
         pointHitRadius: 10,
-        data: [20, 59, 75, 20, 20, 55, 40], // часы (по дням)
+        data: Object.values(dataUserWithDate).map(function (item) {
+          if (item.Reading) {
+            return item.Reading;
+          }
+          return 0;
+        }),
       },
       {
-        label: "Car Down",
+        label: `${Object.keys(dataUserGeneral)[1]}`,
         fill: false,
         lineTension: 0.1,
         backgroundColor: "rgba(75,75,192,0.4)",
@@ -145,7 +200,196 @@ const drawLineDiagram = () => {
         pointHoverBorderColor: "rgba(220,220,220,1)",
         pointRadius: 1,
         pointHitRadius: 10,
-        data: [20, 59, 25, 20, 50, 65, 40], // часы (по дням)
+        data: Object.values(dataUserWithDate).map(function (item) {
+          if (item.Laziness) {
+            return item.Laziness;
+          }
+          return 0;
+        }),
+      },
+      {
+        label: `${Object.keys(dataUserGeneral)[2]}`,
+        fill: false,
+        lineTension: 0.1,
+        backgroundColor: "rgba(150,206,180,0.4)",
+        borderColor: "rgba(150,206,180,1)",
+        borderDash: [],
+        borderJoinStyle: "miter",
+        pointBorderColor: "rgba(150,206,180,1)",
+        pointBackgroundColor: "#fff",
+        pointBorderWidth: 1,
+        pointHoverRadius: 5,
+        pointHoverBackground: "rgba(150,206,180,1)",
+        pointHoverBorderColor: "rgba(150,206,180,1)",
+        pointRadius: 1,
+        pointHitRadius: 10,
+        data: Object.values(dataUserWithDate).map(function (item) {
+          if (item.Relax) {
+            return item.Relax;
+          }
+          return 0;
+        }),
+      },
+      {
+        label: `${Object.keys(dataUserGeneral)[3]}`,
+        fill: false,
+        lineTension: 0.1,
+        backgroundColor: "rgba(75,75,192,0.4)",
+        borderColor: "rgba(75,72,192,1)",
+        borderDash: [],
+        borderJoinStyle: "miter",
+        pointBorderColor: "rgba(75,72,192,1)",
+        pointBackgroundColor: "#fff",
+        pointBorderWidth: 1,
+        pointHoverRadius: 5,
+        pointHoverBackground: "rgba(75, 72,192,1)",
+        pointHoverBorderColor: "rgba(220,220,220,1)",
+        pointRadius: 1,
+        pointHitRadius: 10,
+        data: Object.values(dataUserWithDate).map(function (item) {
+          if (item.Sleeping) {
+            return item.Sleeping;
+          }
+          return 0;
+        }),
+      },
+      {
+        label: `${Object.keys(dataUserGeneral)[4]}`,
+        fill: false,
+        lineTension: 0.1,
+        backgroundColor: "rgba(75,75,192,0.4)",
+        borderColor: "rgba(75,72,192,1)",
+        borderDash: [],
+        borderJoinStyle: "miter",
+        pointBorderColor: "rgba(75,72,192,1)",
+        pointBackgroundColor: "#fff",
+        pointBorderWidth: 1,
+        pointHoverRadius: 5,
+        pointHoverBackground: "rgba(75, 72,192,1)",
+        pointHoverBorderColor: "rgba(220,220,220,1)",
+        pointRadius: 1,
+        pointHitRadius: 10,
+        data: Object.values(dataUserWithDate).map(function (item) {
+          if (item.Working) {
+            return item.Working;
+          }
+          return 0;
+        }),
+      },
+      {
+        label: `${Object.keys(dataUserGeneral)[5]}`,
+        fill: false,
+        lineTension: 0.1,
+        backgroundColor: "rgba(75,75,192,0.4)",
+        borderColor: "rgba(75,72,192,1)",
+        borderDash: [],
+        borderJoinStyle: "miter",
+        pointBorderColor: "rgba(75,72,192,1)",
+        pointBackgroundColor: "#fff",
+        pointBorderWidth: 1,
+        pointHoverRadius: 5,
+        pointHoverBackground: "rgba(75, 72,192,1)",
+        pointHoverBorderColor: "rgba(220,220,220,1)",
+        pointRadius: 1,
+        pointHitRadius: 10,
+        data: Object.values(dataUserWithDate).map(function (item) {
+          if (item.Studing_ENG) {
+            return item.Studing_ENG;
+          }
+          return 0;
+        }),
+      },
+      {
+        label: `${Object.keys(dataUserGeneral)[6]}`,
+        fill: false,
+        lineTension: 0.1,
+        backgroundColor: "rgba(75,75,192,0.4)",
+        borderColor: "rgba(75,72,192,1)",
+        borderDash: [],
+        borderJoinStyle: "miter",
+        pointBorderColor: "rgba(75,72,192,1)",
+        pointBackgroundColor: "#fff",
+        pointBorderWidth: 1,
+        pointHoverRadius: 5,
+        pointHoverBackground: "rgba(75, 72,192,1)",
+        pointHoverBorderColor: "rgba(220,220,220,1)",
+        pointRadius: 1,
+        pointHitRadius: 10,
+        data: Object.values(dataUserWithDate).map(function (item) {
+          if (item.Studing_JS) {
+            return item.Studing_JS;
+          }
+          return 0;
+        }),
+      },
+      {
+        label: `${Object.keys(dataUserGeneral)[7]}`,
+        fill: false,
+        lineTension: 0.1,
+        backgroundColor: "rgba(75,75,192,0.4)",
+        borderColor: "rgba(75,72,192,1)",
+        borderDash: [],
+        borderJoinStyle: "miter",
+        pointBorderColor: "rgba(75,72,192,1)",
+        pointBackgroundColor: "#fff",
+        pointBorderWidth: 1,
+        pointHoverRadius: 5,
+        pointHoverBackground: "rgba(75, 72,192,1)",
+        pointHoverBorderColor: "rgba(220,220,220,1)",
+        pointRadius: 1,
+        pointHitRadius: 10,
+        data: Object.values(dataUserWithDate).map(function (item) {
+          if (item.Watching_FILMS) {
+            return item.Watching_FILMS;
+          }
+          return 0;
+        }),
+      },
+      {
+        label: `${Object.keys(dataUserGeneral)[8]}`,
+        fill: false,
+        lineTension: 0.1,
+        backgroundColor: "rgba(75,75,192,0.4)",
+        borderColor: "rgba(75,72,192,1)",
+        borderDash: [],
+        borderJoinStyle: "miter",
+        pointBorderColor: "rgba(75,72,192,1)",
+        pointBackgroundColor: "#fff",
+        pointBorderWidth: 1,
+        pointHoverRadius: 5,
+        pointHoverBackground: "rgba(75, 72,192,1)",
+        pointHoverBorderColor: "rgba(220,220,220,1)",
+        pointRadius: 1,
+        pointHitRadius: 10,
+        data: Object.values(dataUserWithDate).map(function (item) {
+          if (item.Walking) {
+            return item.Walking;
+          }
+          return 0;
+        }),
+      },
+      {
+        label: `${Object.keys(dataUserGeneral)[9]}`,
+        fill: false,
+        lineTension: 0.1,
+        backgroundColor: "rgba(75,75,192,0.4)",
+        borderColor: "rgba(75,72,192,1)",
+        borderDash: [],
+        borderJoinStyle: "miter",
+        pointBorderColor: "rgba(75,72,192,1)",
+        pointBackgroundColor: "#fff",
+        pointBorderWidth: 1,
+        pointHoverRadius: 5,
+        pointHoverBackground: "rgba(75, 72,192,1)",
+        pointHoverBorderColor: "rgba(220,220,220,1)",
+        pointRadius: 1,
+        pointHitRadius: 10,
+        data: Object.values(dataUserWithDate).map(function (item) {
+          if (item.Playing_Games) {
+            return item.Playing_Games;
+          }
+          return 0;
+        }),
       },
     ],
   };
@@ -200,7 +444,6 @@ btnSend.addEventListener("click" || "keyup", (event) => {
 
     drawLineDiagram(valueSelectedItem, hours);
 
-    localStorage.setItem("dateTime", JSON.stringify(dateFull));
     localStorage.setItem("dataUserGeneral", JSON.stringify(dataUserGeneral));
     localStorage.setItem("dataUserWithDate", JSON.stringify(dataUserWithDate));
 
